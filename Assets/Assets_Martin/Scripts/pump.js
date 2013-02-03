cd115d132713557460d1ee9750372f2b53f47e86
MikaelRS <mikaelrino@hotmail.com>
2013-02-04 00:10:42 +0100

    Added script for interaction with pump/valve

--- Assets/Assets_Martin/Scripts/pump.js
+++ Assets/Assets_Martin/Scripts/pump.js
@@ -1,9 +1,16 @@
 #pragma strict
-
+var svart : GameObject;
+		
 function Start () {
 
 }
 
 function Update () {
-
+	var x = svart.transform.position.x;
+	var z = svart.transform.position.z;
+	if(Mathf.Abs((transform.position.x - x)) < 10 && Mathf.Abs((transform.position.z - z)) < 10) {
+		if(Input.GetButtonDown("Jump")) {
+			//To-do-code whenever space is pressed in the vicinity of pump
+		}
+	}
 }
